6bd383a25383a2de1abb921374c0d1b29f8c24af
Omar Rosas <jorosas@gmail.com>
2020-06-25 20:19:15 -0500

    Ok. Botones get json, leer archivo txt

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 document.getElementById('getText').addEventListener('click', getText);
+document.getElementById('getUsers').addEventListener('click', getUsers);
+
 
 function getText() {
     // ************** Versión 1
@@ -20,7 +22,29 @@ function getText() {
     // Versión 2
     fetch('sample.txt')
         .then((res) => res.text())
-        .then((data) => console.log(data));
-
-
+        .then((data) => {
+            document.getElementById('output').innerHTML = data;
+        })
+        .catch(err => console.log(err));
 }
+
+// Nota: Función para leer archivo json
+function getUsers() {
+    fetch('users.json')
+        .then((res) => res.json())
+        .then((data) => {
+            let output = '<h2>Users</h2>';
+            data.forEach(function (user) {
+                output += `
+                <ul>
+                    <li>ID: ${user.id}</li>
+                    <li>Name: ${user.name}</li>
+                    <li>Email: ${user.email}</li>
+                </ul>
+                `;
+            });
+            document.getElementById('output').innerHTML = output;
+            console.log(data);
+        });
+}
+
